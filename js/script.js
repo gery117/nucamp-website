2bfcf364a4b19ab145372c4faab01641766f9bd9
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-01-06 08:37:54 +0800

    reworked carousel button

--- js/script.js
+++ js/script.js
@@ -6,15 +6,17 @@ const carousel = new bootstrap.Carousel('#homeCarousel', {
 })
 
 // when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
-})
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
-})
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+})   

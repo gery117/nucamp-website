d443d4997fd757e3a8059fe90516dd04513fd20b
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-01-15 04:12:15 +0800

    styling of the weather widget and adding JS

--- js/script.js
+++ js/script.js
@@ -20,3 +20,37 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 })   
+
+const apiKey = process.env.OPEN_WEATHER_API_KEY;
+let city = "Beijing";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+
+async function fetchWeather(){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        displayWeather(data);
+    }
+    catch(error){
+        console.log(`There was an error`, error);
+
+    }
+}
+
+fetchWeather();
+
+let icon = document.querySelector('#weather-icon');
+let temprature = document.querySelector('#weather-temp');
+let weatherDiscr = document.querySelector('#weather-description');
+
+function displayWeather(data){
+    const image = document.createElement('img');
+    image.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    image.alt = "weather image";
+    icon.appendChild(image);
+
+    temprature.textContent =`${data.main.temp} \u00B0 F`;
+    weatherDiscr.textContent = `${data.weather[0].description}`;
+}
